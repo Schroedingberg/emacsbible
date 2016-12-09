029407ef246c06a0572b3551dfac38693791cfd1
Schroedingberg <aaronrebmann@gmail.com>
2016-12-09 18:43:50 +0100

    Add haskell support

--- init.el
+++ init.el
@@ -27,6 +27,8 @@
 (load-file "~/.emacs.d/books/bookofchecking.el")
 (load-file "~/.emacs.d/books/bookofclojure.el")
 (load-file "~/.emacs.d/books/bookofcommonlisp.el")
+(load-file "~/.emacs.d/books/bookofcommonlisp.el")
+(load-file "~/.emacs.d/books/bookofhaskell.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
