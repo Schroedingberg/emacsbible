32b749c235534fe476ff1b7636cfcdeebb740eec
Aaron Rebmann <aaronrebmann@gmail.com>
2016-12-20 18:11:22 +0100

    Remove telephone line, use wombat theme

---
+++ books/bookofcolours.el
@@ -0,0 +1,30 @@
+(use-package rainbow-mode)
+(rainbow-mode)
+;; Colored parentheses
+(use-package rainbow-delimiters)
+;; Start rainbow-delimiters mode in most programming related modes
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; Highlight Tail
+(use-package highlight-tail
+  :init
+  (setq highlight-tail-colors '(("#Ff4500" . 0)
+				 ("#8b0000" . 10)
+				 ("#Ff8c00" . 20)
+				 ("#Ffa500" . 30)
+				 ("#Ffd700" . 40 )
+				 )
+   ))
+(highlight-tail-mode)
+
+
+;; Mode line configuration
+
+(use-package mode-icons)
+(mode-icons-mode)
+
+
+;; Themes
+;(use-package leuven-theme)
+(use-package wombat-theme)
+
