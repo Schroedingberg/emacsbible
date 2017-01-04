0ec65e74146b6f6f5b37d9ea98ce85f7737027c6
Aaron Rebmann <aaronrebmann@gmail.com>
2017-01-04 22:23:51 +0100

    Set up a basic haskell support based on haskell mode.
    - It seems to resemble cider quiet a lot, the repl integration worked
      well at first glance

--- books/bookofhaskell.el
+++ books/bookofhaskell.el
@@ -1,3 +1,21 @@
 (use-package hindent)
 (add-hook 'haskell-mode-hook #'hindent-mode)
 
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t)
+  ;; Use the cabal repl. This might have to be reviewed again
+  '(haskell-process-type 'cabal-repl))
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
