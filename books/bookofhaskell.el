1f2bb23213428f39f159f50e4358f60312949e02
Aaron Rebmann <aaronrebmann@gmail.com>
2017-01-08 13:25:25 +0100

    Add cabal path to haskell setup

--- books/bookofhaskell.el
+++ books/bookofhaskell.el
@@ -1,6 +1,9 @@
 (use-package hindent)
 (add-hook 'haskell-mode-hook #'hindent-mode)
-
+(let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
+  (setenv "PATH" (concat my-cabal-path path-separator (getenv "PATH")))
+  (add-to-list 'exec-path my-cabal-path))
+(custom-set-variables '(haskell-tags-on-save t))
 (custom-set-variables
   '(haskell-process-suggest-remove-import-lines t)
   '(haskell-process-auto-import-loaded-modules t)
