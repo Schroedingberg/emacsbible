7408c1e82b5f9163e7cec235a4e7ad16c55a85ae
Aaron Rebmann <aaronrebmann@gmail.com>
2017-01-09 16:01:04 +0100

    Add ess

--- init.el
+++ init.el
@@ -13,6 +13,11 @@
 ;; Make use-package always resolve dependencies.
 (setq use-package-always-ensure t)
 ;; Read the holy books of configuration
+(defmacro read-book (book)
+  `(concat "~/.emacs.d/books/" ,book)
+  )
+(setq library '(bookofnavigation
+	       ))
 (load-file "~/.emacs.d/books/bookofnavigation.el")
 (load-file "~/.emacs.d/books/bookofcompletion.el")
 (load-file "~/.emacs.d/books/bookofcolours.el")
@@ -30,6 +35,8 @@
 (load-file "~/.emacs.d/books/bookofcommonlisp.el")
 (load-file "~/.emacs.d/books/bookofcommonlisp.el")
 (load-file "~/.emacs.d/books/bookofhaskell.el")
+(load-file "~/.emacs.d/books/bookofbooks.el")
+(load-file "~/.emacs.d/books/bookofstatistics.el")
 ;;(load-file "~/.emacs.d/books/bookofmessages.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -40,6 +47,11 @@
  '(custom-safe-themes
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "01ac390c8aa5476078be3769f3c72a9e1f5820c9d9a8e8fcde21d0ff0bbeeec1" default)))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote cabal-repl))
+ '(haskell-tags-on-save t)
  '(helm-external-programs-associations (quote (("html" . "firefox") ("pdf" . "evince"))))
  '(org-modules
    (quote
@@ -47,7 +59,7 @@
  '(org-use-property-inheritance t)
  '(package-selected-packages
    (quote
-    (color-theme-sanityinc-tomorrow tomorrow-night-light-theme git-timemachine dictcc helm use-package)))
+    (ess sicp color-theme-sanityinc-tomorrow tomorrow-night-light-theme git-timemachine dictcc helm use-package)))
  '(pdf-misc-print-programm "/usr/bin/gtklp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
