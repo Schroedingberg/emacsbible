8c5c45723c9490b059aa96d856991e7a1afbdc5e
Aaron Rebmann <aaronrebmann@gmail.com>
2017-01-15 11:25:37 +0100

    Add rudimentary cask setup to bookofdev

---
+++ books/bookofdev.el
@@ -0,0 +1,10 @@
+(use-package realgud
+  :pin melpa)
+
+
+(use-package cask)
+(use-package cask-mode)
+(use-package caskxy
+  :config
+  (setq caskxy--cask-location "/usr/bin/cask"))
+(provide 'bookofdev)
