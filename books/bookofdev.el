3f233ebbcdfc3a075378546ffd4b7076c389aced
Aaron Rebmann <aaronrebmann@gmail.com>
2017-01-23 10:41:54 +0100

    Add development tools for elisp

--- books/bookofdev.el
+++ books/bookofdev.el
@@ -1,13 +1,18 @@
+
+;; General debugging
 (use-package realgud
   :pin melpa)
 
-
+;; Elisp
 (use-package cask)
 (use-package cask-mode)
 (use-package caskxy
   :config
   (setq caskxy--cask-location "/usr/bin/cask"))
-
-
 (use-package flycheck-package)
+
+;; Python
+
+;; Java
+(use-package meghanada)
 (provide 'bookofdev)
